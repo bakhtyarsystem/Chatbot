dc56d424c8b61661ce3a91fa7b8378daaf19fae9
BakhtyarAhani <bakhtyarsystem@gmail.com>
2025-03-29 16:53:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import flet as ft
+from google import genai
+from google.genai import types
 
+client = genai.Client(api_key='AIzaSyDQnB2xE-NyPBSfukf-uWlcEw42tpwsLuw')
 
 def main(page: ft.Page):
     def btn_click(e):
         if not txt_name.value:
-            txt_name.error_text = "Please enter your name"
+            txt_name.error_text = "Please enter your Question"
             page.update()
         else:
             name = txt_name.value
             page.clean()
-            page.add(ft.Text(f"Hello, {name}!"))
+            instruction = "You are a muslim shia who wants to invite all people to islam shia with kind language."
+            page.add(ft.Text(client.models.generate_content(model="gemini-2.5-pro-exp-03-25",config=types.GenerateContentConfig(system_instruction=instruction), contents=question)))
 
-    txt_name = ft.TextField(label="Your name")
+    txt_name = ft.TextField(label="Your Question")
 
-    page.add(txt_name, ft.ElevatedButton("Say hello!", on_click=btn_click))
+    page.add(txt_name, ft.ElevatedButton("AI answer!", on_click=btn_click))
 
 
 ft.app(main)
